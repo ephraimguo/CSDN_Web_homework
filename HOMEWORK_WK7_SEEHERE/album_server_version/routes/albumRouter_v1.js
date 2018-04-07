76e0afc0ecf620190b60eb0e24738f5690f86751
ephraimguo <ephraimguo@gmail.com>
2018-04-08 02:43:25 +0800

    server album now can read the photos name

--- HOMEWORK_WK7_SEEHERE/album_server_version/routes/albumRouter_v1.js
+++ HOMEWORK_WK7_SEEHERE/album_server_version/routes/albumRouter_v1.js
@@ -10,17 +10,39 @@ var addPhotoMW = multer({
 });
 var addPhoto = addPhotoMW.single("singlePhoto");
 
+var currentAlbum = "default";
 
 
 router.get('/', function(req, res){
-  res.render('album', {photoName, photoList});
+  res.render('album', {photoName, photoList, currentAlbum});
 });
 
 router.post('/addPhotoName', function(req, res) {
-  photoName.push(req.body);
-  console.log(photoName, "------------");
+  const albName = req.body["new-album"];
+  photoName.push(albName);
+  photoList[albName] = [];
+
+  console.log(photoList[albName], '--------');
+  // console.log(photoName, "------------");
   fs.writeFileSync(__dirname + "/photoName.json", JSON.stringify(photoName));
+  fs.writeFileSync(__dirname + "/photoList.json", JSON.stringify(photoList));
+  // fs.writeFileSync(__dirname + "/photoName.json", photoName);
   res.redirect("back");
+});
+
+router.post('/addPhotos', addPhoto, function(req, res){
+  // here need some codes ....
+  photoList[currentAlbum].push(req.file.filename);
+  fs.writeFileSync(__dirname + "/photoList.json", JSON.stringify(photoList));
+  console.log(photoList,"-----------");
+  res.redirect("back");
+});
+
+router.get('/:photoName', function(req, res){
+  // here also need some codes
+  currentAlbum = req.params.photoName;
+  res.redirect("back");
+  // fs.createReadStream(__dirname + "/photosFolder/"+req.params.name).pipe(res);
 });
 
 
