33e2a827bd8aa5f61cc738c41c9fc11dcbfe1e37
ephraimguo <ephraimguo@gmail.com>
2018-04-09 16:57:40 +0800

    server album now can read the photos name

--- HOMEWORK_WK7_SEEHERE/album_server_version/routes/albumRouter_v1.js
+++ HOMEWORK_WK7_SEEHERE/album_server_version/routes/albumRouter_v1.js
@@ -11,43 +11,99 @@ var addPhotoMW = multer({
 var addPhoto = addPhotoMW.single("singlePhoto");
 
 var currentAlbum = "default";
+// const currentAlbumLength = photoList[currentAlbum].length;
+var photoPerPage = 3;
+var currentPage = 1;
+var pageContent = [];
 
-
+function displayPhotoPage(currentPage, currentAlbum){
+  pageContent = [];
+  for(let i=(currentPage-1)*photoPerPage; i<currentPage*photoPerPage; i++) {
+    if (photoList[currentAlbum] && photoList[currentAlbum][i] !== undefined && photoList[currentAlbum][i] !== null) {
+      pageContent.push(photoList[currentAlbum][i]);
+    }
+    else {
+      console.log('no enough pic');
+    }
+  }
+}
+// init
 router.get('/', function(req, res){
-  res.render('album', {albumName, photoList, currentAlbum});
+
+  displayPhotoPage(currentPage, currentAlbum);
+  // pageContent.push(photoList[currentAlbum][i]);
+  res.render('album', {albumName, photoList, currentAlbum, photoPerPage, currentPage, pageContent});
 });
 
+// add new album name
 router.post('/addAlbumName', function(req, res) {
   const albName = req.body["new-album"];
   albumName.push(albName);
   photoList[albName] = [];
 
-  console.log(photoList[albName], '--------');
-  // console.log(albumName, "------------");
   fs.writeFileSync(__dirname + "/photoName.json", JSON.stringify(albumName));
   fs.writeFileSync(__dirname + "/photoList.json", JSON.stringify(photoList));
-  // fs.writeFileSync(__dirname + "/photoName.json", albumName);
   res.redirect("back");
 });
 
+// add photo to designated album
 router.post('/addPhotos', addPhoto, function(req, res){
   // here need some codes ....
   photoList[currentAlbum].push(req.file.filename);
   fs.writeFileSync(__dirname + "/photoList.json", JSON.stringify(photoList));
-  console.log(photoList,"-----------");
+  displayPhotoPage(currentPage, currentAlbum);
   res.redirect("back");
 });
 
-router.get('/:albumName', function(req, res){
-  // here also need some codes
-  fs.createReadStream(__dirname+"/photosFolder/"+req.params.albumName).pipe(res);
+//photoList, right center
+router.get('/show/'+currentAlbum+'/:phName', function(req, res){
+  displayPhotoPage(currentPage, currentAlbum);
+  console.log(req.params.phName, '----', __dirname);
+  fs.createReadStream(__dirname+"/photosFolder/"+req.params.phName).pipe(res);
   // res.redirect("back");
+  // let reader = fs.createReadStream(__dirname+"/photosFolder/"+req.params.phName);
+  // if(photoList[currentAlbum] === "" || photoList[currentAlbum] === undefined || photoList[currentAlbum] === null){
+  //   console.log('nothing to show');
+  //   res.redirect('back');
+  // }
+  // else{
+  //   reader.pipe(res);
+  // }
+
+  // console.log(photoList[currentAlbum], 'xxxxxxxxxxx');
+  // fs.createReadStream(__dirname+"/photosFolder/"+req.params.phName).pipe(res);
 });
 
+
+//album list, on left side
 router.get('/albums/:albumName', function(req, res){
   currentAlbum = req.params.albumName;
-  res.redirect("back");
+  console.log('this is album list router');
+  res.redirect("/album1");
 });
+
+//album pagination links, on right bottom
+router.get('/albums/'+currentAlbum+'/page/:pageNum', function(req, res){
+  // here also need some codes
+  currentPage = Number(req.params.pageNum)+1;
+  console.log(currentPage,'-----page-------','page router');
+  displayPhotoPage(currentPage, currentAlbum);
+  res.redirect("/album1");
+});
+
+// function pagination(){
+//   for(let i = 0; i<currentAlbumLength/photoPerPage; i++) {
+//     let pageNavLink = document.createElement('a');
+//     let pageNavLi = document.createElement('li');
+//     pageNavLink.innerText = `${i + 1}`;
+//     pageNavLink.onclick = function () {
+//       currentPage = i + 1;
+//     };
+//     pageNavLi.appendChild(pageNavLink);
+//     pageNav.appendChild(pageNavLi);
+//   }
+//
+// }
 
 
 module.exports = router;
